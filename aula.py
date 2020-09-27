87f24f59048189d983f5c2c91f5e7b8a7acb45f2
JonthanKBP <jonathan.kevinbp@hotmail.com>
2020-09-27 18:55:20 -0300

    Exe1

--- aula.py
+++ aula.py
@@ -29,6 +29,7 @@ def quantidade_de_amigos (amizades):
 
 def gera_histograma_contagem_amigos(quantidade_de_amigos, qtde_usuarios_na_rede):
   xs = range(qtde_usuarios_na_rede)
+  print(xs)
   ys = [quantidade_de_amigos[x] for x in xs]
   plt.bar(xs, ys)
   plt.axis ([0, qtde_usuarios_na_rede, 0, qtde_usuarios_na_rede /4])
@@ -108,18 +109,68 @@ def desvio_padrao (qtde_amigos):
 #  que mostra a quantidade de amigos que pessoas de 
 # cada sexo têm.
 
-def gera_sexo():
+def define_sexo(quantidade_de_usuarios_na_rede):
+  amizade_sexo = []
+  for _ in range (quantidade_de_usuarios_na_rede):
+    if (random.randint(0, 1) == 0):
+      amizade_sexo.append(('masculino'))
+    else:
+      amizade_sexo.append(('feminino'))
+  return amizade_sexo 
+
+def conta_amizade_sexo(amizades, sexo):
+  total_amigos_sexo = []
+  masculino = 0
+  feminino = 0
+
+  for i in amizades:
+    if sexo[i[0]] == 'masculino':
+      masculino += 1
+    else:
+      feminino += 1
+    if sexo[i[1]] == 'masculino':
+      masculino += 1
+    else:
+      feminino += 1
+
+  total_amigos_sexo.append(masculino)
+  total_amigos_sexo.append(feminino)
+  return total_amigos_sexo
+
+def histograma_sexo_das_amizades(quantidade_de_amigos, qtde_usuarios_na_rede):
+  sexo = define_sexo(qtde_usuarios_na_rede)
+  amizade = conta_amizade_sexo(quantidade_de_amigos, sexo)
+  total = amizade[0] + amizade[1] 
+  xs = (0, 1)
+  ys = [amizade[x] for x in xs]
+  plt.bar(xs, ys)
+  plt.axis(['masculinio', 'feminino', 0, total])
+  plt.yticks ([100 * i for i in range (total//100)])
+  plt.xticks ([i for i in range(-1,3)])
+  plt.xlabel ("Sexo")
+  plt.ylabel ("# de amigos")
+  plt.title ("Quantidades de amigos por sexo")
+  plt.show()
+
+def histograma_sexo_das_amizades_test():
   qtde_usuarios_na_rede = quantidade_de_usuarios_na_rede()
-  amizades = gera_amizades(1000, qtde_usuarios_na_rede)
-  qtde_amigos = quantidade_de_amigos(amizades)
-  gera_histograma_contagem_amigos(qtde_amigos, qtde_usuarios_na_rede)
+  quantidade_de_amigos = gera_amizades(1000, qtde_usuarios_na_rede)
+  histograma_sexo_das_amizades(quantidade_de_amigos, qtde_usuarios_na_rede)
 
+""" 2 Escreva uma função que constrói um histograma que mostra a 
+quantidade de amigos que pessoas de cada idade têm. """
 
-def amigos_por_sexo():
-
+def define_idade(quantidade_de_usuarios_na_rede):
+  idade_usuario = []
+  for _ in range (quantidade_de_usuarios_na_rede):
+    idade_usuario.append(random.randint(12, 120))
+  return idade_usuario 
 
 def main ():
   #mostra_primeiro_e_segundo_maiores_test()
-  media_qtde_amigos_test()
-
+  #media_qtde_amigos_test()
+  #gera_histograma_contagem_amigos_test()
+  #print(conta_amizade_sexo(gera_amizades(1000, 100), define_sexo(100)))
+  histograma_sexo_das_amizades_test()
+  print(define_idade(100))
 main()
